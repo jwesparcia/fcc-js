6e8b9b119af5cfe23ab523723e94bfe10a841aa2
Jhune Wally Esparcia <jhuneesparcia1@gmail.com>
2026-07-21 00:04:55 +0800

    apply discount function

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-distance_mi = 1
-is_raining = False
-has_bike = True
-has_car = True
-has_ride_share_app = False
-
-if distance_mi == 0:
-    print('False')
-
-elif distance_mi <= 1:
-    if is_raining == False:
-        print('True')
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return 'The price should be a number'
+    elif not isinstance(discount, (int, float)):
+        return 'The discount should be a number'
+    elif price <= 0:
+        return 'The price should be greater than 0'
+    elif discount < 0 or discount > 100:
+        return 'The discount should be between 0 and 100'
     else:
-        print('False')
-
-elif distance_mi > 1 and distance_mi <= 6:
-    if has_bike == True and is_raining == False:
-        print('True')
-    else:
-        print('False')
-
-elif distance_mi > 6:
-    if has_car == True or has_ride_share_app == True:
-        print('True')
-    else:
-        print('False')
-
-else:
-    print('True')
+        discount_amount = price * (discount / 100)
+        final_price = price - discount_amount
+        return final_price
