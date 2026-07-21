68bf19c359ad3a09972e9ed94b333b1cb3b58ed7
Jhune Wally Esparcia <jhuneesparcia1@gmail.com>
2026-07-21 15:19:33 +0800

    caesar cipher

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-def apply_discount(price, discount):
-    if not isinstance(price, (int, float)):
-        return 'The price should be a number'
-    elif not isinstance(discount, (int, float)):
-        return 'The discount should be a number'
-    elif price <= 0:
-        return 'The price should be greater than 0'
-    elif discount < 0 or discount > 100:
-        return 'The discount should be between 0 and 100'
-    else:
-        discount_amount = price * (discount / 100)
-        final_price = price - discount_amount
-        return final_price
+def caesar(text, shift, encrypt=True):
+
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+encrypted_text = 'Pbhentr vf sbhaq va hayvxryl cynprf.'
+print(encrypted_text)
+decrypted_text = decrypt(encrypted_text,13)
+
+print(decrypted_text)
