8414c3eaa403c2730afe7e0cff55e82e23977ae7
Jhune Wally Esparcia <jhuneesparcia1@gmail.com>
2026-07-27 21:09:25 +0800

    pin extractor

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def game_score(player1, player2):
-    score1 = 0
-    score2 = 0
+def pin_extractor(poems):
+    secret_codes = []
+    for poem in poems:
+        secret_code = ''
+        lines = poem.split('\n')
+        for line_index, line in enumerate(lines):
+            words = line.split()
+            if len(words) > line_index:
+                secret_code += str(len(words[line_index]))
+            else:
+                secret_code += '0'
+        secret_codes.append(secret_code)
+    return secret_codes        
 
-    for p1, p2 in zip(player1, player2):
-        if p1 == "C" and p2 == "C":
-            score1 += 3
-            score2 += 3
-        elif p1 == "D" and p2 == "D":
-            score1 += 1
-            score2 += 1
-        elif p1 == "D" and p2 == "C":
-            score1 += 5
-        else:  # p1 == "C" and p2 == "D"
-            score2 += 5
+poem = """Stars and the moon
+shine in the sky
+white and
+until the end of the night"""
 
-    return [score1, score2]
+poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
+poem3 = 'There\nonce\nwas\na\ndragon'
+
+print(pin_extractor([poem, poem2, poem3]))
