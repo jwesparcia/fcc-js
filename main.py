f483b7ba576d9df3f2f49e623cf6b4c363171efa
Jhune Wally Esparcia <141032442+jwesparcia@users.noreply.github.com>
2026-07-30 19:51:51 +0800

    number pattern

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-def pin_extractor(poems):
-    secret_codes = []
-    for poem in poems:
-        secret_code = ''
-        lines = poem.split('\n')
-        for line_index, line in enumerate(lines):
-            words = line.split()
-            if len(words) > line_index:
-                secret_code += str(len(words[line_index]))
-            else:
-                secret_code += '0'
-        secret_codes.append(secret_code)
-    return secret_codes        
+def number_pattern(n):    
+    if not isinstance(n, int):
+        return 'Argument must be an integer value.'
+    elif n <= 0:
+        return'Argument must be an integer greater than 0.'
+    else:
+        numbers = []
+        for i in range(1,n+1):
+            numbers.append(str(i))
+        return ' '.join(numbers)
+            
 
-poem = """Stars and the moon
-shine in the sky
-white and
-until the end of the night"""
+print(number_pattern(4))
 
-poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
-poem3 = 'There\nonce\nwas\na\ndragon'
 
-print(pin_extractor([poem, poem2, poem3]))
+    
